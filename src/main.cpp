6f66bfbb776b5630c31dba2e3dbabe671745027f
ElieDaher07 <143806470+ElieDaher07@users.noreply.github.com>
2025-09-30 14:48:29 -0400

    Initialisation

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+// Le code minimal
+
+#include <FastLED.h>
+#include <Arduino.h> 
+#include <MicroOscSlip.h>
+#define MA_BROCHE_BOUTON 39
+#define MA_BROCHE_ANGLE 32
+
+CRGB pixelAtom;
+
+MicroOscSlip<128> monOsc(&Serial);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode( MA_BROCHE_BOUTON , INPUT );
+  FastLED.addLeds<WS2812, 27, GRB>(&pixelAtom, 1); 
+  
+}
+
+void loop() {
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  int maLectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+  
+
+  monOsc.sendInt( "/bouton" , maLectureBouton); // <------ Le nouveau "print" qui remplace serial.print pr le MicroOscSlip
+  monOsc.sendInt( "/analog" , maLectureAnalogique); // Pr le analog
+
+  // Serial.println(maLectureBouton);
+  // On enlève le serial print pcq dès qu'on utilise OSC slip, il faut le faire
+  // info: https://t-o-f.info/aide/#/microosc/initialisation/
+
+  delay(100);
+
+  pixelAtom = CRGB(0, millis() % 255, 0);
+  FastLED.show();
+  
+}
